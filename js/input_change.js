7184fe4f75b26dea8c5504f1a471c9b3c4477026
joshua <jos@sophisticates.de>
2014-09-03 18:14:10 +0200

    punkte updaten, kleine design fixes

--- js/input_change.js
+++ js/input_change.js
@@ -1,3 +1,4 @@
+var dataChanged = false;
 
 // eingabe: spalte welche neu zu berechnen ist.
 function calcPoints(column) {
@@ -14,12 +15,33 @@ $('.editable').each(function(i, tr){
 	$.each(tr.children, function(it, td){
 		// führe calcPoints bei input event aus
 		td.addEventListener('input', function() {
-			var index = $(this).parent().children().index($(this));
-			console.log("hello");
-			var points = calcPoints(index);
-			$(this).parent().siblings(":last").children().eq(index).html(points);
+		dataChanged = true;
+	})
+	});
+});
+
+// gehe alle trs durch
+$('.editable').each(function(i, tr){
+	// gehe alle tds durch
+	$.each(tr.children, function(it, td){
+		// führe calcPoints bei blur event aus
+		td.addEventListener('blur', function() {
+			if (dataChanged){
+				var index = $(this).parent().children().index($(this));
+				var points = calcPoints(index);
+				$(this).parent().siblings(":last").children().eq(index).html(points);
+				dataChanged = false;
+			}
 		})
 	});
 });
+
+// berechne hero und villain
+function honor(){
+	val ind=0;
+	$.each(array, function(i, val) {
+
+	})
+}
 
 
